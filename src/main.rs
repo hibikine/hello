1c7b80a16c59054487fae5d9769dc47025500e7b
Hibikine Kage <goodsmile314159@gmail.com>
2019-08-17 17:33:22 +0900

    hello

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate stdweb;
+
+fn main() {
+    stdweb::initialize();
+
+    let message = "Hello World";
+    js! {
+        console.log( @{message} );
+    }
+
+    stdweb::event_loop();
+}
